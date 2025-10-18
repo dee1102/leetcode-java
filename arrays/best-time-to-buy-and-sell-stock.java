0b338eca6ba43526da9b7f51601c2e2a670654cc
devarshi.vyas <devarshi110297@gmail.com>
2025-10-18 01:17:44 -0400

    feat: dp - easy

--- arrays/best-time-to-buy-and-sell-stock.java
+++ arrays/best-time-to-buy-and-sell-stock.java
@@ -33,35 +33,3 @@ class Solution {
 // Track the lowest price seen so far (min) as you move through the array.
 // At each step, compute the profit if you sell today: prices[i] - min
 // Keep track of the maximum profit seen so far.
-
-
-// Dynamic Programming
-class Solution {
-
-    public int maxProfit(int[] prices) {
-        // if (prices.length == 1) {
-        //     return 0;
-        // }
-        // int[] subProb = Arrays.copyOfRange(prices, 1, prices.length);
-
-        // return Math.max(Arrays.stream(subProb).max().getAsInt() - prices[0], maxProfit(subProb));
-        return recursive(prices, 0).get("result");
-    }
-
-    public HashMap<String, Integer> recursive(int[] prices, int start) {
-        if (start == prices.length - 1) {
-            HashMap<String, Integer> base = new HashMap<>();
-            base.put("result", 0);
-            base.put("max", prices[start]);
-            return base;
-        }
-
-        HashMap<String, Integer> result = recursive(prices, start + 1);
-
-        result.put("result", Math.max(result.get("max") - prices[start], result.get("result")));
-        if (result.get("max") < prices[start]) {
-            result.put("max", prices[start]);
-        }
-        return result;
-    }
-}
